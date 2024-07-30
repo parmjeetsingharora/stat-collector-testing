0bca368250dfb751d171e03b5f7edd9f0a18d7e5
Parmjeet Singh <parmjeetsingh06537@gmail.com>
2024-07-30 11:30:03 +0530

    stat collector testing

---
+++ Test.java
@@ -0,0 +1,17 @@
+public class Test {
+  public static void main(String[] args) {
+    System.out.println("Test 2");
+    System.out.println("Test 3");
+
+    System.out.println("Test 2");
+    System.out.println("Test 3");
+
+
+    System.out.println("Test 2");
+    System.out.println("Test 3");
+
+  }
+
+}
+
+
