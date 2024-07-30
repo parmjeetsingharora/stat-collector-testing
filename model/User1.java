0ac6b178c7ca4edcc0b268f6df0e3e639edb16b1
visheshaggarwal <visheshaggarwal2308@gmail.com>
2024-07-30 15:13:16 +0530

    done

---
+++ model/User1.java
@@ -0,0 +1,14 @@
+package model;
+
+public class User1 {
+  private String name;
+  private int ag1e;
+  private int height2;
+
+  public User1(String name, int ag1e, int height2) {
+    this.name = name;
+    this.ag1e = ag1e;
+    this.height2 = height2;
+  }
+
+}
